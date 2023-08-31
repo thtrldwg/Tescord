5d0822ed49256ef4d35aeae9ca8e4c6d2b2e5237
Ted3x <thatree.ludwig@gmail.com>
2023-08-31 13:09:16 +0200

    Added clerk auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: {children: React.ReactNode } ) => {
     return (
-        <div>
+        <div className="h-full flex items-center justify-center">
             {children}
         </div>
     );
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      Test protected route.
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
