f7c5f61f3c58a23c07f2ee12bd5b6677547eb684
Ted3x <thatree.ludwig@gmail.com>
2023-09-01 11:27:03 +0200

    Next UI dark theme added

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
     </div>
   )
 }
